c10f02da3fe0bc16836d65ec488066e2b0e91e84
frankhart <siddharthadhar.soumen@srmuniv.edu.in>
2021-03-18 17:53:50 +0530

    Update gitignore, add setup

--- mockasm/mockasm.py
+++ mockasm/mockasm.py
@@ -5,7 +5,7 @@ from .utils import file_utils
 
 def run():
     parser = argparse.ArgumentParser(description="Mock ASM")
-    parser.add_argument("--file_path", type="str", default="", help="Path to asm code")
+    parser.add_argument("--file_path", type=str, default="", help="Path to asm code")
     args = parser.parse_args()
 
     source_code = file_utils.read_file(path=args.file_path)
