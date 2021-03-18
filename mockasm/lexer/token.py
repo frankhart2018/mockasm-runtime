58805461f74b0c68d2dd1cac00ce4cc4c4006036
frankhart <siddharthadhar.soumen@srmuniv.edu.in>
2021-03-18 19:51:07 +0530

    Make token members as private and add getters

--- mockasm/lexer/token.py
+++ mockasm/lexer/token.py
@@ -1,8 +1,20 @@
 class Token:
     def __init__(self, lexeme, token_type, line_num):
-        self.lexeme = lexeme
-        self.token_type = token_type
-        self.line_num = line_num
+        self.__lexeme = lexeme
+        self.__token_type = token_type
+        self.__line_num = line_num
+
+    @property
+    def lexeme(self):
+        return self.__lexeme
+
+    @property
+    def token_type(self):
+        return self.__token_type
+
+    @property
+    def line_num(self):
+        return self.__line_num
 
     def __str__(self):
         return f"Token(lexeme=\033[91m{self.lexeme}\033[m, token_type=\033[91m{self.token_type}\033[m, line_num=\033[91m{self.line_num}\033[m)"
--- mockasm/parser/parser.py
+++ mockasm/parser/parser.py
@@ -1,3 +1,12 @@
 class Parser:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, tokens):
+        self.__tokens = tokens
+        self.current_token_ptr = 0
+        self.__opcodes = []
+
+    def __is_token_list_end(self):
+        return self.current_token_ptr >= len(self.__tokens)
+
+    def parse(self):
+        while not self.__is_token_list_end():
+            
